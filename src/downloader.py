84a5da20f2654144017dbf8baf30ece1aa9fee49
Abhijeet Vashistha <abhivashistha5@gmail.com>
2018-05-20 20:25:15 +0530

    hardcoded url removed

--- src/downloader.py
+++ src/downloader.py
@@ -8,8 +8,10 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import os
 import subprocess
+import sys
 
 def get_html(url):
+    '''Function for returning html content of the url as string'''
     if type(url) != type(''):
         raise ValueError('String is expected in url')
     html_string = ''
@@ -18,34 +20,38 @@ def get_html(url):
         html_bytes = response.read()
         html_string = html_bytes.decode('utf-8')
         return html_string
-    except Exception as e:
-        print('Error: Not able to find requested link ' + str(e))
+    except Exception as err:
+        print('Error: Not able to find requested link ' + str(err))
+        exit(1)
 
 def extract_links(html, base_link=None):
+    '''Function to extract the links from html page'''
     html_page = BeautifulSoup(html, 'html.parser')
     all_links = html_page.find_all('a')
     video_links = []
     for link in all_links:
         href = link.get('href')
         name = link.string
-        if type(href) == type('') and href.lower().endswith(('.mp4', '.mkv', '.avi', '.3gp')):
-            if href.startswith(('http://', 'https://')) == False and base_link != None:
-                href = base_link + href
+        if type(href) == type('') and href.lower().endswith(('.mp4', '.mkv', '.avi', '.3gp')): #check if the link points to a video file
+            if href.startswith(('http://', 'https://')) == False and base_link != None: 
+                href = base_link + href # if relative links are used then make absolute link
             video_links.append({'name': name, 'link': href})
     return video_links
 
 
 def main():
-    link = 'http://dl.funsaber.net/serial/Mr.%20Robot/season%201/720/'
-    download_folder = os.path.join(os.getenv('HOME'), 'Downloads')
-    print('download folder ->', download_folder)
-    html = get_html(link)
+    page_link = sys.argv[1]
+    print('Extracting download links from ->', page_link)
+    html = get_html(page_link)
     # print(html)
-    links = extract_links(html, link)
-    print(links)
+    links = extract_links(html, page_link)
     if len(links) == 0:
         print('No links found')
-        exit(1)
+        exit(0)
+    else:
+        print('Found links ->', links)
+    download_folder = os.path.join(os.getenv('HOME'), 'Downloads')
+    print('Download folder ->', download_folder)
     for link in links:
         print('Downloading ->', link['name'])
         try:
